72b212bf839727528ee8e3b948e9e8c846713696
Denison G. Corbal <denison.corbal@gmail.com>
2022-03-10 13:48:06 -0300

    Réplica da aula concluindo a criação das classes do domínio

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -54,14 +55,25 @@ public class Dev {
 	}
 
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-		
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
 	}
 	
 	public void progredir() {
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		
+		if(conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		}
+		else {
+			System.err.println("Você não está matriculado em nenhum conteúdo!");
+		}
 		
 	}
 	
-	public void calcularXp() {
-		
+	public Double calcularTotalXp() {
+		return this.conteudosConcluidos.stream()
+				.mapToDouble(Conteudo :: calcularXp)
+				.sum();
 	}
 }
